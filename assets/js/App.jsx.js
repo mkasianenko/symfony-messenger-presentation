0bd17ad2e72fd9bef8402f986373a7f5241d10c7
Mykhailo Kasianenko <mkasianenko@oroinc.com>
2018-08-13 02:04:43 +0300

    -- symfony/twig-bundle, symfony/webpack-encore-pack

---
+++ assets/js/App.jsx.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+class App extends Component {
+    constructor()
+    {
+        super();
+        this.state ={};
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Code here</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
